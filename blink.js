b7a101289b0f5260c767b8bcdd1c863281ed4177
joseluisn <jonovoan@poligran.edu.co>
2020-05-02 15:19:07 -0500

    sockets, express, johnny five
    Controlar el encendido de un led desde pagina web por el puerto 8080, accediendo desde celular en la misma red wifi

--- blink.js
+++ blink.js
@@ -5,11 +5,11 @@ console.log('Hi Johnny Five from Jose luis');
 // configuramos la tarjeta arduino en una variable
 var board = new five.Board();
 // cuando la tarjeta esta lista ejecutar una funcion.
-board.on("ready", function() {
+board.on("ready", function () {
     console.log('The board is ready!');
-// Declarar el pin 13 en la variable led
-var led = new five.Led(13);
-// Parpadear el led cada 500 milisegundos
-led.blink(500);
-console.log('The led is blinking!');
+    // Declarar el pin 13 en la variable led
+    var led = new five.Led(13);
+    // Parpadear el led cada 500 milisegundos
+    led.blink(500);
+    console.log('The led is blinking!');
 });
--- conexion.js
+++ conexion.js
@@ -1,7 +1,7 @@
-var five = require ('johnny-five');
+var five = require('johnny-five');
 var wifi = require("node-wifi");
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var client = mqtt.connect('mqtt://test.mosquitto.org')
 
 // se crea una nueva board
 var board = new five.Board();
@@ -20,7 +20,7 @@ var data = "";
 /* define L298N o L293D pines del control del motor */
 var leftMotorForward = new five.pin(2);     /* GPIO2(D4) -> IN3   */
 var rightMotorForward = new five.pin(15);   /* GPIO15(D8) -> IN1  */
-var leftMotorBackward =  new five.pin(0);    /* GPIO0(D3) -> IN4   */
+var leftMotorBackward = new five.pin(0);    /* GPIO0(D3) -> IN4   */
 var rightMotorBackward = new five.pin(13);  /* GPIO13(D7) -> IN2  */
 
 
@@ -33,102 +33,102 @@ var leftMotorENB = new five.pin(12);  /* GPIO12(D6) -> Motor-B Enable */
 
 //cuando se inicia la board
 
-board.on('ready',function(){
+board.on('ready', function () {
 
 
     board.repl.inject({
         leftMotorForward: leftMotorForward,
-        rightMotorForward :rightMotorForward,
-        leftMotorBackward :leftMotorBackward,
-         rightMotorBackward:rightMotorBackward
+        rightMotorForward: rightMotorForward,
+        leftMotorBackward: leftMotorBackward,
+        rightMotorBackward: rightMotorBackward
     });
-         five. server.begin();
+    five.server.begin();
 
-         client = server.available();
-         if (!client) return; 
-         data = checkClient ();
+    client = server.available();
+    if (!client) return;
+    data = checkClient();
 
 
 
-         if (data == "forward") MotorForward();
-         /* If the incoming data is "backward", run the "MotorBackward" function */
-         else if (data == "backward") MotorBackward();
-         /* If the incoming data is "left", run the "TurnLeft" function */
-         else if (data == "left") TurnLeft();
-         /* If the incoming data is "right", run the "TurnRight" function */
-         else if (data == "right") TurnRight();
-         /* If the incoming data is "stop", run the "MotorStop" function */
-         else if (data == "stop") MotorStop();
+    if (data == "forward") MotorForward();
+    /* If the incoming data is "backward", run the "MotorBackward" function */
+    else if (data == "backward") MotorBackward();
+    /* If the incoming data is "left", run the "TurnLeft" function */
+    else if (data == "left") TurnLeft();
+    /* If the incoming data is "right", run the "TurnRight" function */
+    else if (data == "right") TurnRight();
+    /* If the incoming data is "stop", run the "MotorStop" function */
+    else if (data == "stop") MotorStop();
 
 });
 
 // hacia adelante
-MotorForward(any)   
+MotorForward(any)
 {
- 
 
-  leftMotorENB.high();
-  rightMotorENB.high();
-  leftMotorForward.high();
-  rightMotorForward.high();
-  rightMotorBackward.low();
-  leftMotorBackward.low();
-  
+
+    leftMotorENB.high();
+    rightMotorENB.high();
+    leftMotorForward.high();
+    rightMotorForward.high();
+    rightMotorBackward.low();
+    leftMotorBackward.low();
+
 }
 
 // Atras
-MotorBackward(any)   
+MotorBackward(any)
 {
-  leftMotorENB.high();
-  rightMotorENB.high();
-  leftMotorForward.high();
-  rightMotorForward.high();
-  rightMotorBackward.low();
-  leftMotorBackward.low();
+    leftMotorENB.high();
+    rightMotorENB.high();
+    leftMotorForward.high();
+    rightMotorForward.high();
+    rightMotorBackward.low();
+    leftMotorBackward.low();
 }
 
 // Voltear a la Izquierda
- TurnLeft(any)   
+TurnLeft(any)
 {
-  
-  leftMotorENB.high();
-  rightMotorENB.high();
-  leftMotorForward.low();
-  rightMotorForward.high();
-  rightMotorBackward.low();
-  leftMotorBackward.higi();
+
+    leftMotorENB.high();
+    rightMotorENB.high();
+    leftMotorForward.low();
+    rightMotorForward.high();
+    rightMotorBackward.low();
+    leftMotorBackward.higi();
 }
 
 //voltear a la derecha
 
-TurnRight(any)   
+TurnRight(any)
 {
-  leftMotorENB.high();
-  rightMotorENB.high();
-  leftMotorForward.high();
-  rightMotorForward.low();
-  rightMotorBackward.high();
-  leftMotorBackward.low();
+    leftMotorENB.high();
+    rightMotorENB.high();
+    leftMotorForward.high();
+    rightMotorForward.low();
+    rightMotorBackward.high();
+    leftMotorBackward.low();
 }
 
 
 //parar
 
- MotorStop(any)   
+MotorStop(any)
 {
-  leftMotorENB.low();
-  rightMotorENB.low();
-  leftMotorForward.low();
-  leftMotorBackward.low();
-  rightMotorForward.low();
-  rightMotorBackward.low();
+    leftMotorENB.low();
+    rightMotorENB.low();
+    leftMotorForward.low();
+    leftMotorBackward.low();
+    rightMotorForward.low();
+    rightMotorBackward.low();
 }
 
- checkClient (any)
+checkClient(any)
 {
-  while(!client.available()) delay(1); 
-  var request = client.readStringUntil('\r');
-  request.remove(0, 5);
-  request.remove(request.length()-9,9);
-  return request;
+    while (!client.available()) delay(1);
+    var request = client.readStringUntil('\r');
+    request.remove(0, 5);
+    request.remove(request.length() - 9, 9);
+    return request;
 }
